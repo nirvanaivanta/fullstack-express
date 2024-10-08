886eca5f9734b5706c040a13984bc6c573aa851a
Vana <nirvanaivanta@gmail.com>
2024-10-09 04:44:10 +0700

    add

---
+++ seeders/20241008212549-demo-products.js
@@ -0,0 +1,26 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('products', [
+      {
+        name: 'Smartphone XYZ',
+        price: 5000000,
+        description: 'Smartphone canggih dengan kamera 48MP',
+        image: '/images/hp.jpg',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Laptop ABC',
+        price: 1500000,
+        description: 'Laptop wireless dengan suara jernih',
+        image: '/images/laptop.jpg',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('products', null, {});
+  },
+};
