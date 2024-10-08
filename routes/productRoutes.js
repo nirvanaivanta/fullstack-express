0af74e0bf7f80f7860a0d168ec1df6d57b23f08b
Vana <nirvanaivanta@gmail.com>
2024-10-09 06:02:56 +0700

    add

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -6,7 +6,7 @@ const Product = require('../config/models/Product');
 router.get('/', async (req, res) => {
   try {
     let products = await Product.findAll();
-    res.render('index', { products });
+    res.render('index', { products, activePage: 'produk' }); 
   } catch (err) {
     console.error('Error fetching products:', err);
     res.status(500).send('Error fetching products');
@@ -15,24 +15,23 @@ router.get('/', async (req, res) => {
 
 // Route untuk menampilkan form tambah produk
 router.get('/add-product', (req, res) => {
-  res.render('add-product');
+  res.render('add-product', { activePage: 'add-product' }); 
 });
 
 // Route untuk menangani penambahan produk
 router.post('/add-product', async (req, res) => {
-    const { name, price, description, image } = req.body;
-    
-    try {
-        // Membuat produk baru dengan data dari form
-        await Product.create({ name, price, description, image });
-        // Redirect ke halaman utama setelah produk berhasil ditambahkan
-        res.redirect('/');
-    } catch (err) {
-        console.error('Error adding product:', err.message); // Log pesan kesalahan
-        res.status(500).send('Error adding product: ' + err.message); // Kirim pesan kesalahan
-    }
+  const { name, price, description, image } = req.body;
+  
+  try {
+    // Membuat produk baru dengan data dari form
+    await Product.create({ name, price, description, image });
+    // Redirect ke halaman utama setelah produk berhasil ditambahkan
+    res.redirect('/');
+  } catch (err) {
+    console.error('Error adding product:', err.message); // Log pesan kesalahan
+    res.status(500).send('Error adding product: ' + err.message); // Kirim pesan kesalahan
+  }
 });
-
 
 // Route untuk membeli produk
 router.post('/buy/:productId', async (req, res) => {
@@ -64,6 +63,17 @@ router.post('/buy/:productId', async (req, res) => {
   } catch (err) {
     console.error('Error buying product:', err);
     res.status(500).json({ message: 'Terjadi kesalahan pada server' });
+  }
+});
+
+// Beranda Route
+router.get('/beranda', async (req, res) => {
+  try {
+    let products = await Product.findAll(); // Ambil data produk dari database
+    res.render('beranda', { products, activePage: 'beranda' }); // Kirim data produk ke beranda.ejs
+  } catch (err) {
+    console.error('Error fetching products:', err);
+    res.status(500).send('Error fetching products');
   }
 });
 
